1cb277a01fb626a148cd29b90ed05135a3fa928b
Necritto <onetopyr@mail.ru>
2020-03-28 22:32:02 +0300

    Feature: auth component [ref:no-ref]

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react'
 import classes from './Auth.module.scss'
+import Button from '../../components/UI/Button/Button'
+import Input from '../../components/UI/Input/Input'
 
 export default class Auth extends Component {
+
+  loginHandler = () => {
+
+  }
+
+  registerHandler = () => {
+
+  }
+
+  onSubmitHandler = e => {
+    e.preventDefault()
+  }
+
   render() {
     return (
       <div className={classes.Auth}>
-        <h1>Auth</h1>
+        <div>
+          <h1>Authorization</h1>
+          <form className={classes.AuthForm} onSubmit={this.onSubmitHandler}>
+            <Input
+              label="Email"
+              type="email"
+            />
+            <Input
+              label="Password"
+              type="password"
+            />
+            <Button type="success" onClick={this.loginHandler}>Sign in</Button>
+            <Button type="primary" onClick={this.registerHandler}>Sign up</Button>
+          </form>
+        </div>
       </div>
     )
   }
