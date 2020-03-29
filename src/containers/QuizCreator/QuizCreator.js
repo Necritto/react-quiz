851610c9d13dba625a8993def9cd70f22edbef81
Necritto <onetopyr@mail.ru>
2020-03-29 22:39:37 +0300

    Feature: component creating quizzes [ref:no-ref]

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,11 +1,99 @@
 import React, { Component } from 'react'
 import classes from './QuizCreator.module.scss'
+import Button from '../../components/UI/Button/Button'
+import Input from '../../components/UI/Input/Input'
+import { createControl } from '../../form/formShared'
+
+function createOptionControl(number) {
+  return createControl({
+    label: `Option ${number}`,
+    errorMessage: 'Value cannot be empty!'
+  }, { required: true })
+}
+
+function createFormControls() {
+  return {
+    question: createControl({
+      label: 'Enter a question',
+      errorMessage: 'The question cannot be empty!'
+    }, { required: true }),
+    option1: createOptionControl(1),
+    option2: createOptionControl(2),
+    option3: createOptionControl(3),
+    option4: createOptionControl(4)
+  }
+}
 
 export default class QuizCreator extends Component {
+
+  state = {
+    quiz: [],
+    formControls: createFormControls()
+  }
+
+  onSubmitHandler = e => {
+    e.preventDefault()
+  }
+
+  addQuestionHandler = () => {
+
+  }
+
+  createQuizHandler = () => {
+
+  }
+
+  changeHandler = (value, controlName) => {
+
+  }
+
+  renderControls() {
+    return Object.keys(this.state.formControls).map((controlName, index) => {
+      const control = this.state.formControls[controlName]
+
+      return (
+        <React.Fragment key={controlName + index}>
+          <Input
+            label={control.label}
+            value={control.value}
+            valid={control.valid}
+            touched={control.touched}
+            errorMessage={control.errorMessage}
+            shouldValidate={!!control.validation}
+            onChange={e => this.changeHandler(e.target.value, controlName)}
+          />
+          {index === 0 ? <hr /> : null}
+        </React.Fragment>
+      )
+    })
+  }
+
   render() {
     return (
       <div className={classes.QuizCreator}>
-        <h1>Quiz Creator</h1>
+        <div>
+          <h1>Quiz Creator</h1>
+
+          <form onSubmit={this.onSubmitHandler}>
+
+            {this.renderControls()}
+
+            <select></select>
+
+            <Button
+              type="primary"
+              onClick={this.addQuestionHandler}
+            >
+              Add question
+            </Button>
+            <Button
+              type="success"
+              onClick={this.createQuizHandler}
+            >
+              Create quiz
+            </Button>
+          </form>
+        </div>
       </div>
     )
   }
